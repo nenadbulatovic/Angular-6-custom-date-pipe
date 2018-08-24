806f80baf8c306e5b965a09699c6bd01688cec6f
Nenad Bulatovic <nenad.bulatovic@gmail.com>
2018-08-24 21:50:41 +0200

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
-import {MatDatepickerInputEvent} from '@angular/material/datepicker';
+import { Component, ViewChild } from '@angular/core';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { MatInput } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -8,22 +9,29 @@ import {MatDatepickerInputEvent} from '@angular/material/datepicker';
 })
 export class AppComponent {
   title = 'Assigment 5 by Nenad Bulatovic';
+  
 
-  elapsedTime;
-  currentDate = new Date();
+  userInput = new Date();
+  showElapsed = false;
 
   constructor() {
-		this.elapsedTime = new Date();
-	}
-	
-	resetLastUpdated() {
-    this.elapsedTime = new Date();
+    this.userInput = new Date();
   }
-  
+
+  @ViewChild('input', { read: MatInput }) input: MatInput;
+  ngAfterViewInit() { }
+
+  resetDate() {
+    this.input.value = '';
+    this.userInput = new Date();
+    this.showElapsed = false;
+  }
+
   events: string[] = [];
 
   addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
-    this.elapsedTime = new Date(event.value);
+    this.userInput = new Date(event.value);
+    this.showElapsed = true;
   }
 
 }
